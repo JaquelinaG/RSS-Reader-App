8f3cf12c4841ef66cdc1d72f54f0efbe9c3a7f26
Jaquelina Giuliani <JG13766@ksoft.com.ar>
2019-12-20 16:37:40 -0300

    New component for Page not found.

--- rss-reader-app/src/app/app.routes.ts
+++ rss-reader-app/src/app/app.routes.ts
@@ -1,11 +1,14 @@
 import { Routes, RouterModule } from "@angular/router";
 import { FeedsComponent } from "./feeds/feeds.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 
 export const routes: Routes = [
-    {
-        path: 'feeds/:category',
-        component: FeedsComponent
-      },
+  {
+    path: 'feeds/:category',
+    component: FeedsComponent
+  },
+  { path: '', redirectTo: '/feeds/:category', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 export const routing = RouterModule.forRoot(routes);
